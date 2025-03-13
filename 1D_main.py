96b5f4bd5bb80d993626a0fd956016522e2408fd
axnj2 <52408308+axnj2@users.noreply.github.com>
2025-03-13 20:17:40 +0100

    working implementation of the conductivity

--- 1D_main.py
+++ 1D_main.py
@@ -26,7 +26,7 @@ epsilon_r = np.ones((M))
 
 # set the local conductivity array
 sigma = np.zeros((M))
-sigma[int(3 * M / 4) - 25 : int(3 * M / 4) + 25] = 1e-1
+sigma[int(3 * M / 4) - 25 : int(3 * M / 4) + 25] = 1e-2
 
 # derived parameters
 DELTA_X = c_vide / (FREQ_REF * 40)  # in meters
@@ -42,7 +42,7 @@ print("DELTA_X : ", DELTA_X, "DELTA_T : ", DELTA_T)
 E0 = np.zeros((M))
 B_tilde_0 = np.zeros((M))
 # intiialise the current density
-J = np.zeros((Q, M))
+J_source = np.zeros((Q, M))
 
 # initialise with a gaussian pulse
 # E0[int(M / 2) - 100 : int(M / 2) + 100] = np.exp(
@@ -51,7 +51,7 @@ J = np.zeros((Q, M))
 
 # set a sinusoidal current at the middle of the grid
 fraction_on = 0.2
-J[0 : int(Q * fraction_on), round(M / 2)] = (
+J_source[0 : int(Q * fraction_on), round(M / 2)] = (
     1
     / 10
     * np.sin(
@@ -75,7 +75,7 @@ B_tilde[0, :] = B_tilde_0[:]
 # this means that we computing step n for E we use step n-1 for B
 # we also consider B_tilde's grid half a step ahead of E's grid in the space domain
 # this means that x=0 for E <-> x=0.5 for B_tilde
-def forward_E(E: np.array, B_tilde: np.array, J: np.array, q: int):
+def forward_E(E: np.array, B_tilde: np.array, J_source: np.array, q: int):
     """
     modifies E in place at step q
     q : int : has to be between 1 and Q-1
@@ -85,7 +85,8 @@ def forward_E(E: np.array, B_tilde: np.array, J: np.array, q: int):
         + (1 / (epsilon_r[1 : M - 1] * e0 * u0))
         * (DELTA_T / ((1 / np.sqrt(epsilon_r[1 : M - 1] * e0 * u0)) * DELTA_X))
         * (B_tilde[q - 1, 1 : M - 1] - B_tilde[q - 1, 0 : M - 2])
-        - (DELTA_T / (epsilon_r[1 : M - 1] * e0)) * J[q-1, 1 : M - 1]
+        - (DELTA_T / (epsilon_r[1 : M - 1] * e0))
+        * [J_source[q - 1, 1 : M - 1] + sigma[1 : M - 1] * E[q - 1, 1 : M - 1]]
     )
 
     # limit conditions :
@@ -108,20 +109,11 @@ def forward_B_tilde(E: np.array, B_tilde: np.array, q: int):
     B_tilde[q, M - 1] = B_tilde[q - 2, M - 2]
     B_tilde[q, 0] = B_tilde[q - 2, 1]
 
-def forward_J(E: np.array, B_tilde: np.array, J: np.array, q: int):
-    """
-    modifies J in place at step q
-    q : int : the time step : has to be between 1 and Q-1
-    """
-    # the first term is the source term that was defined earlier
-    J[q, 1 : M - 1] = J[q, 1 : M - 1] + sigma[1 : M - 1] * E[q-1, 1 : M - 1]
-
 
 def main():
     for q in range(1, Q):
-        forward_E(E, B_tilde, J, q)
+        forward_E(E, B_tilde, J_source, q)
         forward_B_tilde(E, B_tilde, q)
-        forward_J(E, B_tilde, J, q)
 
 
 # %%
@@ -139,11 +131,11 @@ ax1.set_ylabel("E (V/m)")
 ax1.set_title("1D FDTD simulation")
 ax1.tick_params(axis="y", labelcolor="b")
 (lineE,) = plt.plot(x, E[0], label="0 s", color="b")
-(lineJ,) = plt.plot(x, J[0], label="0 s", color="g")
+(lineJ,) = plt.plot(x, J_source[0], label="0 s", color="g")
 plt.legend()
 plt.ylim(
-    -10,#np.min(E, axis=None),
-    10 #np.max(E, axis=None) + 0.1 * (np.max(E, axis=None) - np.min(E, axis=None)),
+    np.min(E, axis=None),
+    np.max(E, axis=None) + 0.1 * (np.max(E, axis=None) - np.min(E, axis=None)),
 )
 
 # show the relative permittivity on the plot
@@ -159,7 +151,7 @@ frame_devider = 1
 def updatefig(i):
     lineE.set_ydata(E[i * frame_devider])
     lineE.set_label(f"{i * frame_devider * DELTA_T:.2e} s")
-    lineJ.set_ydata(J[i * frame_devider])
+    lineJ.set_ydata(J_source[i * frame_devider])
     plt.legend()
     return (lineE, lineJ)
 
