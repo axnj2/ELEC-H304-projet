963cb84e00d8dd1308cb9ab1417de7e7deca046e
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2025-05-16 21:38:20 +0200

    extracted the lasagna definition

--- 2D/simu_chap_7_micro_wave_uniform_lasagna.py
+++ 2D/simu_chap_7_micro_wave_uniform_lasagna.py
@@ -1,118 +1,34 @@
 import numpy as np
 
 from yee_FDTD_2D import (
-    e0,
-    u0,
-    C_VIDE,
     compute_electric_field_amplitude_and_plot,
     get_exponential_decay_alpha,
-)
-from simu_elements import (
-    sinusoïdal_point_source,
-    create_square,
-    meat_relative_real_permittivity,
-    meat_relative_complex_permittivity,
-    pasta_relative_complex_permittivity,
-    pasta_relative_real_permittivity,
-    cheese_relative_complex_permittivity,
-    cheese_relative_real_permittivity,
-    sauce_relative_complex_permittivity,
-    sauce_relative_real_permittivity,
 )
 
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 
+from define_lasagna_microwave import initialize_mixed_lasagna
+
+
+
 # parameters
-# settings parameters
-microwave_side_length = 0.357  # in meters
-FREQ_REF = 1.8e9  # Hz
-Q = 15000  # number of time samples
-TOTAL_CURRENT = 0.01  # A
-LASAGNA_WITH = 0.15  # in meters
-LASAGNA_LENGTH = 0.2  # in meters
+(
+    DELTA_T,
+    DELTA_X,
+    Q,
+    M,
+    FREQ_REF,
+    local_relative_permittivity,
+    local_conductivity,
+    source,
+    LASAGNA_LENGTH,
+    LASAGNA_WITH,
+    microwave_side_length,
+    mixed_lasagna_rel_permittivity,
+    mixed_lasagna_conductivity,
+) = initialize_mixed_lasagna()
 SLICE_POSITION = microwave_side_length / 2 + LASAGNA_WITH / 4
-
-# derived parameters
-WAVE_LENGTH = C_VIDE / FREQ_REF  # in meters
-REFINEMENT_FACTOR = 400  # number of samples per wavelength (min 20)
-DELTA_X = WAVE_LENGTH / REFINEMENT_FACTOR  # in meters
-DELTA_T = 1 / (2 * FREQ_REF * REFINEMENT_FACTOR)  # in seconds
-M = int(microwave_side_length / DELTA_X)  # number of space samples per dimension
-print(f"M : {M}, total number of points : {M**2}")
-
-mixed_lasagna_rel_permittivity = float(
-    np.mean(
-        (
-            meat_relative_real_permittivity,
-            cheese_relative_real_permittivity,
-            pasta_relative_real_permittivity,
-            sauce_relative_real_permittivity,
-        )
-    )
-)
-mixed_lasagna_conductivity = float(
-    np.mean(
-        (
-            meat_relative_complex_permittivity,
-            cheese_relative_complex_permittivity,
-            pasta_relative_complex_permittivity,
-            sauce_relative_complex_permittivity,
-        )
-    )
-    * (2 * np.pi * FREQ_REF * e0)
-)
-speed_of_light_in_lasagna = 1 / np.sqrt(mixed_lasagna_rel_permittivity * e0 * u0)
-print(f"speed_of_light_in_lasagna : {speed_of_light_in_lasagna:.2e} m/s")
-print(f"sample per wavelength : {speed_of_light_in_lasagna / (DELTA_X * FREQ_REF)}")
-print(f"lasagna conductivity : {mixed_lasagna_conductivity:.2e} S/m")
-print(f"thermal inertia : {(1010.25*3670):.2e} J/Km^3")
-
-# add the lasagna in the middle of the grid
-# using the average of the relative permittivity of the ingredients
-# (imagine that the lasagna was mixed before being put in the microwave)
-local_relative_permittivity = create_square(
-    (
-        microwave_side_length / 2 - LASAGNA_LENGTH / 2,
-        microwave_side_length / 2 - LASAGNA_WITH / 2,
-    ),
-    LASAGNA_LENGTH,
-    LASAGNA_WITH,
-    (M, M),
-    DELTA_X,
-    value=mixed_lasagna_rel_permittivity,
-    default_value=1.0,
-)
-
-
-
-
-local_conductivity = create_square(
-    (
-        microwave_side_length / 2 - LASAGNA_LENGTH / 2,
-        microwave_side_length / 2 - LASAGNA_WITH / 2,
-    ),
-    LASAGNA_LENGTH,
-    LASAGNA_WITH,
-    (M, M),
-    DELTA_X,
-    value=mixed_lasagna_conductivity,
-    default_value=0.0,
-)
-
-
-def source(q, J):
-    sinusoïdal_point_source(
-        J,
-        q,
-        M,
-        int(0.320 / DELTA_X),
-        M // 2,
-        TOTAL_CURRENT,
-        FREQ_REF,
-        DELTA_T,
-        DELTA_X,
-    )
 
 
 # initialize the arrays
@@ -208,4 +124,4 @@ ax2.axhline(
 ax2.legend()
 plt.tight_layout()
 plt.savefig("images/microwave_lasagna.png", bbox_inches="tight", dpi=300)
-#plt.show()
+# plt.show()
