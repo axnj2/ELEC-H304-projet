37548015bf14ebc71f9b4a15f11ef88668e1043f
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2025-05-26 19:45:36 +0200

    Failed the trilateration throught the wall

--- 2D/simu_bonus_trilateration_with_lossless_wall.py
+++ 2D/simu_bonus_trilateration_with_lossless_wall.py
@@ -30,17 +30,17 @@ from scipy.optimize import minimize
 
 
 # parameters
-Q = 1700  # number of time samples
-WIDTH = 25  # [m]
+Q = 4000  # number of time samples
+WIDTH = 35  # [m]
 FREQ_REF = 1e9  # [Hz] reference frequency
 TOTAL_CURRENT = 0.01  # [A] total current
-SOURCE_POS = (WIDTH / 2, WIDTH / 2 )  # [m] position of the source
-DETECTOR_1_POS = (WIDTH / 2 - 5, WIDTH / 2)  # [m] position of the detector 1
+SOURCE_POS = (WIDTH / 2+3, WIDTH / 2 )  # [m] position of the source
+DETECTOR_1_POS = (WIDTH / 2 - 6, WIDTH / 2)  # [m] position of the detector 1
 # DETECTOR_2_POS = (WIDTH / 2, WIDTH / 2 - 5) # [m] position of the detector 2
 # DETECTOR_3_POS = (WIDTH/2, WIDTH/2 + 5) # [m] position of the detector 3
 WALL_REL_PERMITTIVITY = brick_wall_rel_permittivity  # relative permittivity of the wall
 WALL_X_END = WIDTH / 2 - 3  # position of the wall
-WALL_THICKNESS = 0.1  # thickness of the wall in meters
+WALL_THICKNESS = 0.5  # thickness of the wall in meters
 
 # derived parameters
 WAVE_LENGTH = C_VIDE / FREQ_REF  # in meters
@@ -54,7 +54,7 @@ print(
     f"actal source position : {round(SOURCE_POS[0] / DELTA_X) * DELTA_X}, {round(SOURCE_POS[1] / DELTA_X) * DELTA_X}"
 )
 
-speed_of_light_in_wall = np.sqrt(1 / (u0 * e0 * brick_wall_rel_permittivity))
+speed_of_light_in_wall = np.sqrt(1 / (u0 * e0 * WALL_REL_PERMITTIVITY))
 print(
     f"number of time steps per wavelength in wall : {speed_of_light_in_wall / (DELTA_X * FREQ_REF):.2f}"
 )
@@ -63,6 +63,7 @@ print(
 # constants
 Z_0 = np.sqrt(u0 / e0)
 beta = 2 * np.pi * FREQ_REF / C_VIDE
+beta_wall = 2 * np.pi * FREQ_REF / speed_of_light_in_wall  # propagation constant in the wall
 Z_2 = np.sqrt(u0 / (e0 * WALL_REL_PERMITTIVITY))  # characteristic impedance in the wall
 
 
@@ -73,6 +74,12 @@ def get_reflexion_coeff(Z1: float, Z2: float, theta_i, theta_t) -> float:
 
 def get_transmission_coeff(Z1: float, Z2: float, theta_i, theta_t) -> float:
     return (2 * Z2 * np.cos(theta_i)) / (Z2 * np.cos(theta_i) + Z1 * np.cos(theta_t))
+
+def get_total_transmission_coeff(theta_i:float, theta_t:float, s:float) -> float:
+    reflection_coeff = get_reflexion_coeff(Z_0, Z_2, theta_i, theta_t)
+
+    denom = np.abs(1 - reflection_coeff * np.exp(-2j * beta_wall * s) * np.exp(1j*beta*2*s*np.sin(theta_t)*np.sin(theta_i)))
+    return np.abs(1-reflection_coeff**2) / denom
 
 def source_func(q: int, current_J) -> None:
     sinusoïdal_point_source(
@@ -108,7 +115,7 @@ E_amp = compute_electric_field_amplitude(
     local_rel_permittivity=wall_rel_perm,
 )
 
-figure, ax = plt.subplots(1, 1, figsize=(6, 6))
+figure, ax = plt.subplots(1, 1, figsize=(8, 8))
 
 plot_field(
     ax,
@@ -121,12 +128,15 @@ def distance_from_source(E_amplitude: float) -> float:
     return Z_0**2 * (beta / (8 * np.pi)) * TOTAL_CURRENT**2 / (E_amplitude**2)
 
 
-def distance_from_source_through_wall(E_amplitude: float) -> float:
+def distance_from_source_through_wall_all_reflexions(E_amplitude: float) -> float:
     """Calculate the distance from the source to the detector through the wall."""
-    transmission_coeff = get_transmission_coeff(Z_0, Z_2, 0, 0)
-    reflection_coeff = get_reflexion_coeff(Z_0, Z_2, 0, 0)
-    distance = transmission_coeff**2 * (1- reflection_coeff**2) * Z_0**2 * (beta / (8 * np.pi)) * TOTAL_CURRENT**2 / (E_amplitude**2)
-    return distance
+    return (
+        get_total_transmission_coeff(0, 0, WALL_THICKNESS) ** 2
+        * Z_0**2
+        * (beta / (8 * np.pi))
+        * TOTAL_CURRENT**2
+        / (E_amplitude**2)
+    )
     
 
 
@@ -136,6 +146,7 @@ def add_detector(
     color: str,
     E_amp,
     distance_func=distance_from_source,
+    name = None,
 ) -> None:
     """Add a detector to the plot."""
     # Show the position of the detectors
@@ -144,6 +155,7 @@ def add_detector(
         M / 100,
         color=color,
         fill=True,
+        label=name,
     )
     ax.add_patch(detector_pos)
 
@@ -160,8 +172,20 @@ def add_detector(
     )
     ax.add_patch(circle)
 
+print(f"total transmission coefficient : {get_total_transmission_coeff(0, 0, WALL_THICKNESS)}")
+add_detector(
+    ax,
+    DETECTOR_1_POS,
+    "red",
+    E_amp,
+    distance_func=distance_from_source_through_wall_all_reflexions,
+    name="Taking into account all reflexions",
+)
+add_detector(ax, DETECTOR_1_POS, "blue", E_amp, distance_func=distance_from_source, name="free space")
+real_distance = np.sqrt((DETECTOR_1_POS[0] - SOURCE_POS[0]) ** 2 + (DETECTOR_1_POS[1] - SOURCE_POS[1]) ** 2)
+print(f"predicted electric field amplitude at detector 1 : {np.abs(get_total_transmission_coeff(0, 0, WALL_THICKNESS)) * Z_0 *np.sqrt(beta/(8*np.pi)) * TOTAL_CURRENT/np.sqrt(real_distance)}")
+print(f"real electric field amplitude at detector 1 : {E_amp[round(DETECTOR_1_POS[1]/DELTA_X), round(DETECTOR_1_POS[0]/DELTA_X)]}")
 
-add_detector(ax, DETECTOR_1_POS, "red", E_amp, distance_func=distance_from_source_through_wall)
 # add_detector(ax, DETECTOR_2_POS, "blue", E_amp)
 # add_detector(ax, DETECTOR_3_POS, "green", E_amp)
 
@@ -198,5 +222,7 @@ add_detector(ax, DETECTOR_1_POS, "red", E_amp, distance_func=distance_from_sourc
 # print(f"estimated source position : {x_source_est}, {y_source_est}")
 # print(f"actual source position : {SOURCE_POS[0]}, {SOURCE_POS[1]}")
 # print(f"estimation error : {np.sqrt((x_source_est - SOURCE_POS[0])**2 + (y_source_est - SOURCE_POS[1])**2)}")
-
+plt.legend()
+plt.title("Trilateration with lossless wall")
+plt.savefig("images/simu_bonus_trilateration_with_lossless_wall_all_reflexion_failed.png", dpi=300, bbox_inches="tight")
 plt.show()
